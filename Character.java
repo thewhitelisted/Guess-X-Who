d26a87a135fc910c376f57a3b75b0278a3325663
thewhitelisted <clee2k6@outlook.com>
2023-12-08 18:03:15 -0500

    feat: create character class

---
+++ Character.java
@@ -0,0 +1,13 @@
+public class Character {
+    // Character defining attributes
+    public String strName;
+    public String strHair;
+    public String strEyes;
+    public String strColour;
+
+    // Character constructor
+    public Character(String name) {
+        // TODO: if statement to determine which character is being created
+        strName = name;
+    }
+}
