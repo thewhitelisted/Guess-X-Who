f5f93700434c6bd2bd2e8dd3098d894730deae31
PoonieWoonie <125479068+PoonieWoonie@users.noreply.github.com>
2023-12-19 13:03:03 -0500

    feat: loads images in the grid. Added submit button in question panel

--- game/GamePanel.java
+++ game/GamePanel.java
@@ -2,7 +2,12 @@ package game;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel{
@@ -10,14 +15,26 @@ public class GamePanel extends JPanel{
     // Each image can be 120 x 120, with 20 size gap between and at the edges
     int intXPos = 20;
     int intYPos = 20;
+
+    InputStream r1c1input = null;
+    InputStream r1c2input = null;
+    InputStream r1c3input = null;
+    InputStream r1c4input = null;
+    InputStream r1c5input = null;
+    BufferedImage r1c1 = null;
+    BufferedImage r1c2 = null;
+    BufferedImage r1c3 = null;
+    BufferedImage r1c4 = null;
+    BufferedImage r1c5 = null;
     
     public void paintComponent(Graphics g){
         g.setColor(Color.black);
         for (int x = 0; x < 5; x++){
-            intYPos = 0;
+            intYPos = 20;
             for(int y = 0; y < 5; y++){
                 //g.fillRect(intXPos, intYPos, 120, 120);
-                System.out.println(intXPos);
+                g.drawImage(r1c1, intXPos, intYPos, null);
+                //System.out.println(intXPos);
                 intYPos += 140;
             }
             intXPos += 140;
@@ -25,6 +42,37 @@ public class GamePanel extends JPanel{
     }
 
     public GamePanel(){
-        
+        r1c1input = this.getClass().getResourceAsStream("img/poon.png");
+        r1c2input = this.getClass().getResourceAsStream("img/poon.png");
+        r1c3input = this.getClass().getResourceAsStream("img/poon.png");
+        r1c4input = this.getClass().getResourceAsStream("img/poon.png");
+        r1c5input = this.getClass().getResourceAsStream("img/poon.png");
+        try{
+            r1c1 = ImageIO.read(r1c1input);
+            r1c2 = ImageIO.read(r1c2input);
+            r1c3 = ImageIO.read(r1c3input);
+            r1c4 = ImageIO.read(r1c4input);
+            r1c5 = ImageIO.read(r1c5input);
+        }catch(IOException e){
+            // if poonpicture does not load, load from folder
+            try {
+                r1c1 = ImageIO.read(new File("img/poon.png"));
+                r1c2 = ImageIO.read(new File("img/poon.png"));
+                r1c3 = ImageIO.read(new File("img/poon.png"));
+                r1c4 = ImageIO.read(new File("img/poon.png"));
+                r1c5 = ImageIO.read(new File("img/poon.png"));
+            } catch (IOException e1) {
+            } 
+        } catch (IllegalArgumentException e2){
+            // if poonpicture does not load, load from folder
+            try {
+                r1c1 = ImageIO.read(new File("img/poon.png"));
+                r1c2 = ImageIO.read(new File("img/poon.png"));
+                r1c3 = ImageIO.read(new File("img/poon.png"));
+                r1c4 = ImageIO.read(new File("img/poon.png"));
+                r1c5 = ImageIO.read(new File("img/poon.png"));
+            } catch (IOException e1) {
+            } 
+        }
     }
 }
