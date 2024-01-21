565391c259474a7a17238c011c7742cf4bba8d09
thewhitelisted <clee2k6@outlook.com>
2024-01-21 09:55:15 -0500

    fix: change jframe to panel

--- game/PickFrame.java
+++ game/PickFrame.java
@@ -2,11 +2,10 @@ package game;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class PickFrame extends JFrame{
+public class PickFrame extends JPanel{
     JPanel panel;
     JLabel title = new JLabel("Pick a character");
     JButton pick = new JButton("Pick");
@@ -14,18 +13,8 @@ public class PickFrame extends JFrame{
     JComboBox<String> characters = new JComboBox<String>(strCharacterQuestions);
 
     public PickFrame(){
-        panel = new JPanel();
-        panel.setLayout(null);
-        panel.setBounds(0, 0, 800, 600);
-
-        // add components
-        panel.add(title);
-        panel.add(pick);
-        panel.add(characters);
-
-        this.add(panel);
-        this.setSize(800, 600);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
+        this.add(title);
+        this.add(pick);
+        this.add(characters);
     }
 }
