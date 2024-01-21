c74f49bc750ba8c44d0a88103c4f5f92fbe2be90
thewhitelisted <clee2k6@outlook.com>
2024-01-21 09:43:25 -0500

    fix: wrong branch

--- network/SuperSocketListener.java
+++ network/SuperSocketListener.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionListener;
 
 import game.Game;
 import game.Main;
-import game.PickFrame;
 import game.QuestionPanel;
 
 public class SuperSocketListener implements ActionListener {
@@ -61,7 +60,6 @@ public class SuperSocketListener implements ActionListener {
                     System.out.println(counter);
                 } else if (Integer.parseInt(strMessage.substring(0, 1)) == START) {
                     Main.chat_box.append("[SYS] Game started." + "\n");
-                    Main.main_frame.setContentPane(new PickFrame());
                 }
             }
         }
