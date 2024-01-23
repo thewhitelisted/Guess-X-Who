9b3856c952210f1959e8a895065ca3aaeb59e868
thewhitelisted <clee2k6@outlook.com>
2024-01-23 13:59:15 -0500

    fix: fixing problems

--- network/SuperSocketListener.java
+++ network/SuperSocketListener.java
@@ -83,7 +83,9 @@ final public class SuperSocketListener implements ActionListener {
             strquestioninfo[1] = args[3];
             if (strquestioninfo[0].equals("Character") && !this.blnServer) {
                 // client is player1
+                System.out.println(strquestioninfo[1] + " " + Game.player1.strName);
                 if (Game.checkGuess(Game.getCharFromName(strquestioninfo[1]), Game.player2)) {
+                    System.out.println("test");
                     Main.chat_box.append("[SYS] User: " + args[1] + " guessed correctly." + "\n");
                     Main.chat_box.append("[SYS] Game ended." + "\n");
                     ssm.sendText(CHAT + "," + "[SYS] User: " + args[1] + " guessed correctly." + "\n");
